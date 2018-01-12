36d7c2ab9fa6daca977f4d2c74dbfcc3536579b1
Jason Ihaia <jason@ihaia.com>
2018-01-12 11:53:08 -0700

    fixed title

--- template/src/components/core/AppBar.vue
+++ template/src/components/core/AppBar.vue
@@ -2,7 +2,7 @@
 <v-toolbar :color="theme" dark app clipped-left fixed>
   <v-toolbar-title :style="$vuetify.breakpoint.smAndUp ? 'width: 300px; min-width: 250px' : 'min-width: 72px'" class="ml-0 pl-3">
     <v-toolbar-side-icon @click.stop="toggleNavigator"></v-toolbar-side-icon>
-    <span class="hidden-xs-only">\{{ title }</span>
+    <span class="hidden-xs-only">\{{ title }}</span>
   </v-toolbar-title>
   <div class="d-flex align-center" style="margin-left: auto">
     <v-btn icon>
